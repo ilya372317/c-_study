3a0b01aea49672fc7e6331d285b8c2ed4d998f23
Otinov <ilya.otinov@gmail.com>
2022-11-23 21:19:45 +0700

    sdf

---
+++ src/chapter_12/test_ios.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ilya on 23.11.22.
+//
+
+#include "iostream"
+
+using namespace std;
+
+int main() {
+    cout.setf(ios::right);
+    cout << "this text placed in right side!" << endl;
+}
