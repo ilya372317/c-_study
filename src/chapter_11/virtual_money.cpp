cd713612837e355e69b47d3f01c64976686f9895
Отинов Илья <ilya@MBP-Otinov.RM.ru>
2022-11-23 21:20:10 +0700

    start chapter_12

--- src/chapter_11/virtual_money.cpp
+++ src/chapter_11/virtual_money.cpp
@@ -10,11 +10,9 @@ using namespace std;
 
 class BigMoney {
 private:
-    long double money;
-public:
-    BigMoney(): money(0) {}
+    long double money{};
 
-    explicit BigMoney(char* s) {
+    void convertStringToMoney(char* s) {
         char* copyStr = new char [strlen(s) + 1];
         char* copyStrStart = copyStr;
 
@@ -33,11 +31,166 @@ public:
 
         delete [] copyStrStart;
     }
+public:
+    BigMoney(): money(0) {}
+
+    explicit BigMoney(char* s) {
+        this->convertStringToMoney(s);
+    }
+
+    BigMoney(long double m) {
+        money = m;
+    }
 
     long double getMoney() const {
         return money;
     }
+
+    char* moneyToString() const {
+        string moneyString = to_string(money);
+
+        int positionOfDot = 0;
+
+        for (auto& item:moneyString) {
+            if (item == '.') {
+                break;
+            }
+            positionOfDot++;
+        }
+
+        bool afterDotOnlyZeroDigits = true;
+
+        const char* cMoneyString = moneyString.c_str();
+
+        for (int i = 0; i < strlen(cMoneyString); i++) {
+            if (i > positionOfDot && *(cMoneyString + i) != '0') {
+                afterDotOnlyZeroDigits = false;
+            }
+        }
+
+        if (afterDotOnlyZeroDigits) {
+            auto* bufferMoneyString = new char [positionOfDot];
+
+            for (int i = 0; i < positionOfDot; i++) {
+                *(bufferMoneyString + i) = *(cMoneyString + i);
+            }
+
+            bufferMoneyString += '\0';
+            cMoneyString = bufferMoneyString;
+        }
+
+        unsigned long countOfDots = (positionOfDot + 1) / 3;
+        char* resultString = new char [strlen(cMoneyString) + countOfDots + 2];
+        int n = 0;
+
+        for (unsigned long i = strlen(cMoneyString), k = strlen(cMoneyString) + countOfDots;
+        i > 0;
+        i--, k--) {
+            if ((n % 3) == 0 && i < positionOfDot) {
+                *(resultString + (k - 1)) = '.';
+                k--;
+            }
+
+            n++;
+
+            *(resultString + (k - 1)) = *(cMoneyString + (i - 1));
+
+            if (i == 1 && (k -1) > 0) {
+                *(resultString + (k - 2)) = *(cMoneyString + (i - 1));
+            }
+        }
+
+        resultString += '\0';
+
+        return resultString;
+    }
+
+    static BigMoney add(BigMoney m1, BigMoney m2) {
+        long double resultMoney = m1.money + m2.money;
+        return {resultMoney};
+    }
+
+    static BigMoney minus(BigMoney m1, BigMoney m2) {
+        long double result = m1.money - m2.money;
+        return {result};
+    }
+
+    static BigMoney separate(BigMoney m1, BigMoney m2) {
+        long double result = m1.money / m2.money;
+        return {result};
+    }
+
+    static BigMoney multiply(BigMoney m1, BigMoney m2) {
+        long double result = m1.money * m2.money;
+        return {result};
+    }
+
+    friend BigMoney operator+(BigMoney, BigMoney);
+    friend BigMoney operator-(BigMoney, BigMoney);
+    friend BigMoney operator*(BigMoney, BigMoney);
+    friend BigMoney operator/(BigMoney, BigMoney);
 };
+
+BigMoney operator+(BigMoney m1, BigMoney m2) {
+    return BigMoney::add(m1, m2);
+}
+
+BigMoney operator-(BigMoney m1, BigMoney m2) {
+    return BigMoney::minus(m1, m2);
+}
+
+BigMoney operator/(BigMoney m1, BigMoney m2) {
+    return BigMoney::separate(m1, m2);
+}
+
+BigMoney operator*(BigMoney m1, BigMoney m2) {
+    return BigMoney::multiply(m1, m2);
+}
+
+TEST(BigM, testMinusOperator) {
+    BigMoney m1 = 5.0 - 2.0;
+    EXPECT_DOUBLE_EQ(3.0, m1.getMoney());
+}
+
+TEST(BigM, testSeparateOpepator) {
+    BigMoney m1 = 6.0 / 2.0;
+    EXPECT_DOUBLE_EQ(3.0, m1.getMoney());
+}
+
+TEST(BigM, testMultiplyOperator) {
+    BigMoney m1 = 2.0 * 5.0;
+    EXPECT_DOUBLE_EQ(10.0, m1.getMoney());
+}
+
+TEST(BigM, testPlusOperator) {
+    BigMoney m1 = 30.0 + 20.0;
+    EXPECT_DOUBLE_EQ(50.0, m1.getMoney());
+}
+
+TEST(BigM, testConvertMoneyFromDotString) {
+    char digitString[] = "1234654";
+    auto* bigMoneyPtr = new BigMoney(digitString);
+    char* moneyString = bigMoneyPtr->moneyToString();
+
+    EXPECT_STREQ("1.234.654", moneyString);
+}
+
+TEST(BigM, testVeryLongConvert) {
+    char digitString[] = "1000111222333444";
+    auto* bigMoneyPtr = new BigMoney(digitString);
+    char* moneyString = bigMoneyPtr->moneyToString();
+
+    EXPECT_STREQ("1.000.111.222.333.444", moneyString);
+}
+
+TEST(BigM, testConvertMoneyToString) {
+    char digitString[] = "5000";
+    auto* bigMoneyPtr = new BigMoney(digitString);
+    char* moneyString = bigMoneyPtr->moneyToString();
+
+    EXPECT_STREQ("5.000", moneyString);
+
+}
 
 TEST(BigM, testConstructorWithDigitString) {
     char digitString[] = "3000000000";
@@ -55,6 +208,13 @@ TEST(BigM, testConstructorWithNonDigitString) {
     EXPECT_DOUBLE_EQ(3000.25, bigMoneyPtr->getMoney());
 
     delete bigMoneyPtr;
+}
+
+TEST(BigM, testConstructorWithVeryLongString) {
+    char digitString[] = "$3000000000000000";
+    auto* bigMoneyPtr = new BigMoney(digitString);
+
+    EXPECT_DOUBLE_EQ(3000000000000000, bigMoneyPtr->getMoney());
 }
 
 int main() {
---
+++ src/chapter_12/first_io_script.cpp
@@ -0,0 +1,106 @@
+//
+// Created by Отинов Илья on 21.11.2022.
+//
+
+#include "iostream"
+#include "gtest/gtest.h"
+#include "cstring"
+
+using namespace std;
+
+class A {
+private:
+    int data;
+public:
+    A(): data(0) {}
+
+    explicit A(int d): data(d) {}
+
+    void showData() const {
+        cout << this->getData() << endl;
+    }
+
+    int getData() const {
+        return data;
+    }
+
+    virtual const char* getClassData() const {
+        return "null";
+    };
+
+    const char* getDataString() const {
+        const char* userString = this->getClassData();
+        char* tempUserString = new char [strlen(userString)];
+        strcpy(tempUserString, userString);
+        string dataString = to_string(this->getData());
+        const char* dataCString = dataString.c_str();
+        strcat(tempUserString, dataCString);
+
+        char* result = new char [strlen(userString) + 1];
+        strcpy(result, tempUserString);
+        result += '\0';
+
+        return result;
+    }
+};
+
+class B : public A {
+public:
+    B (): A(0) {}
+
+    explicit B (int d): A(d) {}
+
+    const char* getClassData() const override {
+        return "data in class B ";
+    }
+};
+
+class C : public A {
+public:
+    C () : A(0) {}
+
+    explicit C (int d): A(d) {}
+
+    const char* getClassData() const override {
+        return "data in class C ";
+    }
+};
+
+TEST(IoTest, InheritTest) {
+    auto* b = new B(10);
+    auto* c = new C(20);
+
+    EXPECT_EQ(10, b->getData());
+    EXPECT_EQ(20, c->getData());
+}
+
+TEST(IoTest, stringTest) {
+    auto* b = new B(10);
+    auto* c = new C(20);
+    const char* bString = b->getDataString();
+    const char* cString = c->getDataString();
+
+    EXPECT_STREQ("data in class B 10", bString);
+    EXPECT_STREQ("data in class C 20", cString);
+
+    delete [] bString;
+    delete [] cString;
+}
+
+void makeNewFile();
+
+void writeDataInFile(char*, char*);
+
+int main() {
+    ::testing::InitGoogleTest();
+
+    return RUN_ALL_TESTS();
+}
+
+void makeNewFile() {
+
+}
+
+void writeDataInFile(char* destFile, char* data) {
+
+}
